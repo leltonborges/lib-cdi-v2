32e574152ecabc43571374b2d1b09a6942f24955
Lelton Borges <leltondev@gmail.com>
2021-10-11 11:51:20 -0300

    create interceptor

--- src/main/java/br/com/cdi/api/lib/transaction/ManagerTransaction.java
+++ src/main/java/br/com/cdi/api/lib/transaction/ManagerTransaction.java
@@ -1,23 +1,33 @@
 package br.com.cdi.api.lib.transaction;
 
 import javax.inject.Inject;
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.Interceptor;
 import javax.interceptor.InvocationContext;
 import javax.persistence.EntityManager;
 import java.io.Serializable;
 
-//@Interceptor
-//@TransactionCDI
+@Interceptor
+@TransactionCDI
 public class ManagerTransaction implements Serializable {
     private static final long serialVersionUID = 1l;
 
     @Inject
     private EntityManager manager;
 
-//    @AroundInvoke
-    public Object getManagerTransaction(InvocationContext context) throws Exception{
+    @AroundInvoke
+    public Object getManagerTransaction(InvocationContext context) {
         manager.getTransaction().begin();
+        try {
         Object result = context.proceed();
-        manager.getTransaction().begin();
-        return  result;
+        manager.getTransaction().commit();
+        return result;
+        }catch (Exception e){
+            manager.getTransaction().rollback();
+            throw new RuntimeException("Erro durante o prcesso getManagerTransaction\n"+ e);
+        }
     }
 }
+
+
+
